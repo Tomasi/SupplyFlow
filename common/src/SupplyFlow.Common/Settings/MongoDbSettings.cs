c3c6647dc291da8c25bc7f2c7f86cca5a5b02c4a
Igor Tomasi <igortomasi@outlook.com>
2023-11-17 21:23:04 -0300

    Ajustes app settings

--- common/src/SupplyFlow.Common/Settings/MongoDbSettings.cs
+++ common/src/SupplyFlow.Common/Settings/MongoDbSettings.cs
@@ -2,7 +2,5 @@ namespace SupplyFlow.Common.Settings;
 
 public class MongoDbSettings
 {
-    public string? Host { get; init; }
-    public int Port { get; init; }
-    public string ConnectionString => $"mongodb://{Host}:{Port}";
+    public string ConnectionString { get; set; }
 }
