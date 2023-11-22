7745d32324fdde9b5b7e9b7bb627ecb2ff2c858e
Igor Tomasi <igortomasi@outlook.com>
2023-11-21 23:13:15 -0300

    Ajustes

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -13,11 +13,11 @@ function App()
     <Router>
       <Navbar />
       <Routes>
-        <Route path="/home" element={<Home />}></Route>
-        <Route path="/estoque" element={<Estoque />}></Route>
-        <Route path="/pedidos" element={<Pedidos />}></Route>
-        <Route path="/entrada" element={<Entrada />}></Route>
-        <Route path="/saida" element={<Saida />}></Route>
+        <Route path="SupplyFlow/home" element={<Home />}></Route>
+        <Route path="SupplyFlow/estoque" element={<Estoque />}></Route>
+        <Route path="SupplyFlow/pedidos" element={<Pedidos />}></Route>
+        <Route path="SupplyFlow/entrada" element={<Entrada />}></Route>
+        <Route path="SupplyFlow/saida" element={<Saida />}></Route>
       </Routes>
     </Router>
   )
