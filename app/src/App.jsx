af41aa2f565bb9f008e208e0fee06ca6de035f2e
Igor Tomasi <igortomasi@outlook.com>
2023-11-21 23:33:12 -0300

    Ajustes rotas para do lado do cliente

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -5,22 +5,40 @@ import Pedidos from "./components/pedidosCompra"
 import Entrada from "./components/entrada"
 import Saida from "./components/saida"
 
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 function App()
 {
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/home" element={<Home />}></Route>
-        <Route path="/estoque" element={<Estoque />}></Route>
-        <Route path="/pedidos" element={<Pedidos />}></Route>
-        <Route path="/entrada" element={<Entrada />}></Route>
-        <Route path="/saida" element={<Saida />}></Route>
-      </Routes>
-    </Router>
-  )
+    <BrowserRouter>
+      <Switch>
+        {"/home"}
+      </Switch>
+      <Switch>
+        {"/estoque"}
+      </Switch>
+      <Switch>
+        {"/pedidos"}
+      </Switch>
+      <Switch>
+        {"/entrada"}
+      </Switch>
+      <Switch>
+        {"/saida"}
+      </Switch>
+    </BrowserRouter>
+  );
+  // <Router>
+  //   <Navbar />
+  //   <Routes>
+  //     <Route path="/home" element={<Home />}></Route>
+  //     <Route path="/estoque" element={<Estoque />}></Route>
+  //     <Route path="/pedidos" element={<Pedidos />}></Route>
+  //     <Route path="/entrada" element={<Entrada />}></Route>
+  //     <Route path="/saida" element={<Saida />}></Route>
+  //   </Routes>
+  // </Router>
+  // )
 }
 
 export default App
