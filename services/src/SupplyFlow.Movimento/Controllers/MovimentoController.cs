b8068839e21d600767b08f433ef0989e50c3cc99
Tomasi <igortomasi@outlook.com>
2023-12-03 20:44:29 -0300

    Incluído namaspace padrão para controller de movimento

--- services/src/SupplyFlow.Movimento/Controllers/MovimentoController.cs
+++ services/src/SupplyFlow.Movimento/Controllers/MovimentoController.cs
@@ -6,15 +6,17 @@ using SupplyFlow.Contracts;
 using SupplyFlow.Movimento;
 using SupplyFlow.Service.Dtos;
 
+namespace SupplyFlow.Service.Movimento.Controllers;
+
 [ApiController]
 [Route("movimento")]
 public class MovimentoController : ControllerBase
 {
-    private readonly IRepository<Movimento> _entityRepository;
+    private readonly IRepository<Common.Entities.Movimento> _entityRepository;
     private readonly IRepository<Produto> _entityRepositoryProduto;
     private readonly IPublishEndpoint _publishEndPoint;
 
-    public MovimentoController(IRepository<Movimento> entityRepository, IRepository<Produto> entityRepositoryProduto, IPublishEndpoint publishEndpoint)
+    public MovimentoController(IRepository<Common.Entities.Movimento> entityRepository, IRepository<Produto> entityRepositoryProduto, IPublishEndpoint publishEndpoint)
     {
         this._entityRepository = entityRepository;
         this._entityRepositoryProduto = entityRepositoryProduto;
@@ -24,7 +26,7 @@ public class MovimentoController : ControllerBase
     [HttpPost]
     public async Task<ActionResult> PostAsync(CreateMovimentoDto createMovimentoDto)
     {
-        Movimento movimento = createMovimentoDto.AsMovimento();
+        Common.Entities.Movimento movimento = createMovimentoDto.AsMovimento();
         movimento.Produto = await _entityRepositoryProduto.GetAsync(createMovimentoDto.Produto);
         await _entityRepository.CreateAsync(movimento);
         await _publishEndPoint.Publish(new MovimentoCreated(movimento.Id));
